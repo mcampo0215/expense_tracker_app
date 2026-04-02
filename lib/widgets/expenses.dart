21c16b5cae6430c5b3783a01610331122a39dcd9
mcampo0215 <matthewcampoverde@gmail.com>
2026-04-02 11:18:40 -0400

    Added top app bar in expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -34,6 +34,13 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expense Tracker'),
+        actions: [
+          IconButton(icon: const Icon(Icons.add), 
+          onPressed:(){})
+        ]
+      ),
       body: Column(
         children: [
           Text('CHART GOES HERE'),
