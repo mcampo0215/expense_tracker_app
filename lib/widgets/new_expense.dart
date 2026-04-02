d44c443f36b1d499da63a31614f7d63e5a40097d
mcampo0215 <matthewcampoverde@gmail.com>
2026-04-02 12:08:37 -0400

    added amount controller

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -3,37 +3,48 @@ import 'package:flutter/material.dart';
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
 
-  State<NewExpense> createState() {
-    return _NewExpenseState();
-  }
+  State<NewExpense> createState() => _NewExpenseState();
 }
 
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
-
+  final _amountController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(16),
+      padding: EdgeInsetsGeometry.all(16),
       child: Column(
-        children:[
+        children: [
           TextField(
             controller: _titleController,
             maxLength: 50,
             keyboardType: TextInputType.name,
             decoration: InputDecoration(
+            
               label: Text('Title'),
             ),
           ),
-          Row(children: [
+          TextField(
+            controller: _amountController,
+            maxLength: 10,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              prefixText: '\$', 
+              label: Text('Amount'),
+            ),
+          ),
 
-          ],),
-          
-          Row(children: [
-            ElevatedButton(onPressed: (){
-              print(_titleController.text);
-            }, child: Text('Save Expense'))
-          ])
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                  print(_amountController.text);
+                },
+                child: Text("Save Event"),
+              ),
+            ],
+          ),
         ],
       ),
     );
