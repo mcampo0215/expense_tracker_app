2d1efb2ed1090e46b1b6532f3ddedefbbc34983a
mcampo0215 <matthewcampoverde@gmail.com>
2026-04-14 12:13:00 -0400

    Started theming

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,33 @@
 import 'package:expense_tracker_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
 void main() {
-  runApp(const MaterialApp(home: const Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 20,
+          ),
+        ),
+        colorScheme: kColorScheme, 
+        appBarTheme: AppBarTheme(
+          backgroundColor: kColorScheme.onPrimaryContainer, 
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+      ),
+      home: Expenses(),
+  ));
 }
