62770918e5f745ad806052be7fe2c7d975dfa344
mcampo0215 <matthewcampoverde@gmail.com>
2026-04-16 11:50:34 -0400

    Added chart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -6,6 +6,7 @@ const uuid = Uuid();
 final formatter = DateFormat.yMd();
 
 enum Category { food, travel, leisure, work }
+
 const categoryIcons = {
   Category.food: Icons.breakfast_dining,
   Category.travel: Icons.flight_takeoff,
@@ -29,5 +30,24 @@ class Expense {
 
   String get formattedDate {
     return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses = allExpenses
+          .where((expense) => expense.category == category)
+          .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
   }
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 
 import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widgets/chart/chart.dart';
 import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker_app/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
@@ -90,7 +91,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          Text('CHART GOES HERE'),
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: mainContent
           ),
